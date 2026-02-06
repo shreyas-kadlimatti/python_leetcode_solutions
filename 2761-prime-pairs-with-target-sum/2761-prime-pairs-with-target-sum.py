2b63800254c33857c2c121ffafe768d2b8ea68c9
shreyas kadlimatti <shreyas.kadlimatti9@gmail.com>
2026-02-06 22:12:13 +0530

    Enhance code with comments on prime pair logic
    Added comments explaining the algorithm's time complexity and alternative approach.

--- 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.py
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.py
@@ -1,4 +1,6 @@
 class Solution:
+#using Eratosthenes. Time-complexity-(n(log(logn))) and 
+# mertens second therome-partial sum of prime num till n is (log(logn))
     def findPrimePairs(self, n: int) -> List[List[int]]:
         
         prime=[True]*(n+1)
@@ -10,6 +12,17 @@ class Solution:
         for i in range(2,(n//2+1)):
                 if prime[i] and prime[n-i]:
                     l.append([i,n-i])
-        return (l)           
+        return (l)         
+
+#other approach 
         
-                
+    # prime=[True]*(n+1)
+    # for i in range(2,int(n**0.5)+1):
+    #     if prime[i]==True:
+    #         for j in range(i*i,n+1,i):
+    #             prime[j]=False 
+    # l=[]
+    # for i in range(2,(n//2+1)):
+    #         if prime[i] and prime[n-i]:
+    #             l.append([i,n-i])
+    # return (l)  
