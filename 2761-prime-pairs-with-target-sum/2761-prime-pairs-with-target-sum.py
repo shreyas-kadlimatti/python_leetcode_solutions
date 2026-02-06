83080994b7f62934826891bf56f77af1949eed64
shreyas kadlimatti <shreyas.kadlimatti9@gmail.com>
2026-02-06 22:12:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.py
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.py
@@ -1,28 +1,37 @@
 class Solution:
-#using Eratosthenes. Time-complexity-(n(log(logn))) and 
-# mertens second therome-partial sum of prime num till n is (log(logn))
     def findPrimePairs(self, n: int) -> List[List[int]]:
-        
+        def p():
+            # prime=[True]*(n+1)
+            # for i in range(2,int(n**0.5)+1):
+            #     if prime[i]==True:
+            #         for j in range(i*i,n+1,i):
+            #             prime[j]=False 
+            # l=[]
+            # for i in range(2,(n//2+1)):
+            #         if prime[i] and prime[n-i]:
+            #             l.append([i,n-i])
+            # return (l)  
         prime=[True]*(n+1)
         for i in range(2,int(n**0.5)+1):
             if prime[i]==True:
                 for j in range(i*i,n+1,i):
                     prime[j]=False 
-        l=[]
-        for i in range(2,(n//2+1)):
-                if prime[i] and prime[n-i]:
-                    l.append([i,n-i])
-        return (l)         
+        pnums=[]
+        for i in range(2,len(prime)) :
+            if prime[i]==True:
+                pnums.append(i)
+        res=[]     
+        l=0 
+        r=len(pnums)-1
 
-#other approach 
-        
-    # prime=[True]*(n+1)
-    # for i in range(2,int(n**0.5)+1):
-    #     if prime[i]==True:
-    #         for j in range(i*i,n+1,i):
-    #             prime[j]=False 
-    # l=[]
-    # for i in range(2,(n//2+1)):
-    #         if prime[i] and prime[n-i]:
-    #             l.append([i,n-i])
-    # return (l)  
+        while l<=r:
+            if pnums[l]+pnums[r]==n:
+                res.append([pnums[l],pnums[r]])
+                l=l+1 
+                r+1
+            elif pnums[l]+pnums[r]>n:
+                r=r-1
+            else:
+                l+=1
+                
+        return res
